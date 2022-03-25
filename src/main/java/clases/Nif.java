c8579fb65098b84dcf8ebe6527e9a167992d92a5
alvaro <alvaromarflo@gmail.com>
2022-03-25 12:10:11 +0100

    aaaa

--- src/main/java/clases/Nif.java
+++ src/main/java/clases/Nif.java
@@ -31,12 +31,81 @@ public class Nif {
         return fechaCaducidad;
     }
 
-    /*
     
     //metodo que genera la letra
-    private char generarLetra(){
-        
-        
+    private char generarLetra(String numero){
+        char letra;
+        switch (Integer.parseInt(numero) % 23) {
+            case 0:
+                letra = 'T';
+                break;
+            case 1:
+                letra = 'R';
+                break;
+            case 2:
+                letra = 'W';
+                break;
+            case 3:
+                letra = 'A';
+                break;
+            case 4:
+                letra = 'G';
+                break;
+            case 5:
+                letra = 'M';
+                break;
+            case 6:
+                letra = 'Y';
+                break;
+            case 7:
+                letra = 'F';
+                break;
+            case 8:
+                letra = 'P';
+                break;
+            case 9:
+                letra = 'D';
+                break;
+            case 10:
+                letra = 'X';
+                break;
+            case 11:
+                letra = 'B';
+                break;
+            case 12:
+                letra = 'N';
+                break;
+            case 13:
+                letra = 'J';
+                break;
+            case 14:
+                letra = 'Z';
+                break;
+            case 15:
+                letra = 'S';
+                break;
+            case 16:
+                letra = 'Q';
+                break;
+            case 17:
+                letra = 'V';
+                break;
+            case 18:
+                letra = 'H';
+                break;
+            case 19:
+                letra = 'L';
+                break;
+            case 20:
+                letra = 'C';
+                break;
+            case 21:
+                letra = 'K';
+                break;
+            default:
+                letra = 'E';
+                break;
+        }
         return letra;
     }
     
@@ -47,7 +116,6 @@ public class Nif {
     }
     
     
-     */
     @Override
     public String toString() {
         return "Nif:" + "numero=" + numero + ", letra=" + letra + ", fechaCaducidad=" + fechaCaducidad;
